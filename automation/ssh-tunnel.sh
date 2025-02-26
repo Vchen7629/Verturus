bb2f84468e68a2be76876cfed2e18edf6f81f27d
Vchen7629 <chenvincent7629@gmail.com>
2025-02-25 23:12:09 -0800

    set up autossh to create ssh tunnel that runs in the background

--- automation/ssh-tunnel.sh
+++ automation/ssh-tunnel.sh
@@ -6,10 +6,10 @@ RED='\u001b[31m'
 NC='\033[0m'
 
 echo -e "${YELLOW}Installing dependencies for autossh...${NC}"
-sudo apt-get autossh
+sudo apt-get install autossh
 echo -e "${GREEN}Autossh installed Succesfully...${NC}"
 
 echo -e "${YELLOW}Preparing to set up ssh tunnel...${NC}"
-ssh -R 127.0.0.1:9090:localhost:9090 vchen7629@
+autossh -M 0 -N -R 127.0.0.1:9090:localhost:9090 vchen7629@164.92.97.22
 
 
